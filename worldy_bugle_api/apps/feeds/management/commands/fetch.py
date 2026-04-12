84297f83fb2d1c26aa23ae79c90ce9d5dea80aa6
vo-olivier250405 <oliviervo.pro@gmail.com>
2026-04-12 15:34:25 +0200

    "feat: article creation with country"

--- worldy_bugle_api/apps/feeds/management/commands/fetch.py
+++ worldy_bugle_api/apps/feeds/management/commands/fetch.py
@@ -1,6 +1,5 @@
-from pprint import pprint
-
 from apps.articles.detector import DetectorUtils
+from apps.articles.models import Article, Country
 from apps.feeds.fetcher import Fetcher
 from apps.feeds.models import Source
 from django.core.management.base import BaseCommand
@@ -10,12 +9,39 @@ class Command(BaseCommand):
     help = "Test the Fetcher class"
 
     def handle(self, *_args, **_kwargs):
+        # Article.objects.all().delete()
+        # Country.objects.all().delete()
+
         source = Source.objects.first()
         fetcher = Fetcher(source)
         detector = DetectorUtils(source)
         entries = fetcher.get_entries()
+
+        self.stdout.write(f"Fetched {len(entries)} entries")
+
         for entry in entries:
-            country = detector.detect_country(entry)
-            print("\n")
-            pprint(country)
-            pprint(entry.get("title", "No title"))
+            codes_names = detector.detect_country(entry)
+            published_at = detector.detect_published_date(entry)
+
+            self.stdout.write(f"\nProcessing entry: {entry.get('title', '')}")
+            self.stdout.write(f"Detected country codes: {codes_names}")
+
+            article, created = Article.objects.get_or_create(
+                title=entry.get("title", ""),
+                resume=entry.get("summary", ""),
+                url=entry.get("link", ""),
+                published_at=published_at,
+                source=source,
+            )
+            if not created:
+                self.stdout.write(f"{article.title} skipped.")
+                continue
+
+            code_to_set = []
+            for code in codes_names:
+                country, _ = Country.objects.get_or_create(
+                    code=code["code"], defaults={"name": code["name"]}
+                )
+                code_to_set.append(country)
+
+            article.countries.set(code_to_set)
