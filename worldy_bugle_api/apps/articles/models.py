3200752310348ec8e09e6d0f4fa066b503cc099e
vo-olivier250405 <oliviervo.pro@gmail.com>
2026-04-12 15:34:03 +0200

    "feat: add country model for many to many relation with article"

--- worldy_bugle_api/apps/articles/models.py
+++ worldy_bugle_api/apps/articles/models.py
@@ -3,11 +3,19 @@ import uuid
 from django.db import models
 
 
-class Articles(models.Model):
+class Country(models.Model):
+    code = models.CharField(max_length=3, primary_key=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.code
+
+
+class Article(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     resume = models.TextField()
-    country = models.CharField(max_length=3)
+    countries = models.ManyToManyField(Country, related_name="articles")
     url = models.URLField()
     published_at = models.DateTimeField()
     source = models.ForeignKey("feeds.Source", on_delete=models.CASCADE)
